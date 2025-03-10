de75faf3499b4b21d4c9321df624ef88a2faa7fd
ACL-404 <zxcsd0919313688@gmail.com>
2025-03-10 16:00:19 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace _004
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAllClear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtCM = new System.Windows.Forms.TextBox();
+            this.txtM = new System.Windows.Forms.TextBox();
+            this.txtKM = new System.Windows.Forms.TextBox();
+            this.txtIn = new System.Windows.Forms.TextBox();
+            this.txtFt = new System.Windows.Forms.TextBox();
+            this.txtYard = new System.Windows.Forms.TextBox();
+            this.BT1 = new System.Windows.Forms.Label();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.BT2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAllClear
+            // 
+            this.btnAllClear.BackColor = System.Drawing.SystemColors.Info;
+            this.btnAllClear.Font = new System.Drawing.Font("Bradley Hand ITC", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAllClear.Location = new System.Drawing.Point(519, 356);
+            this.btnAllClear.Name = "btnAllClear";
+            this.btnAllClear.Size = new System.Drawing.Size(201, 82);
+            this.btnAllClear.TabIndex = 0;
+            this.btnAllClear.Text = "DELETE";
+            this.btnAllClear.UseVisualStyleBackColor = false;
+            this.btnAllClear.Click += new System.EventHandler(this.btnAllClear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(34, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "單位轉換";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(65, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "公分";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(320, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 30);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "公尺";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(611, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 30);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "公里";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("新細明體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(65, 242);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 30);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "英吋";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("新細明體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.Location = new System.Drawing.Point(320, 242);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 30);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "英尺";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("新細明體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label7.Location = new System.Drawing.Point(611, 242);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 30);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "英碼";
+            // 
+            // txtCM
+            // 
+            this.txtCM.Location = new System.Drawing.Point(37, 180);
+            this.txtCM.Name = "txtCM";
+            this.txtCM.Size = new System.Drawing.Size(133, 25);
+            this.txtCM.TabIndex = 8;
+            this.txtCM.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtCM_KeyUp);
+            // 
+            // txtM
+            // 
+            this.txtM.Location = new System.Drawing.Point(289, 180);
+            this.txtM.Name = "txtM";
+            this.txtM.Size = new System.Drawing.Size(133, 25);
+            this.txtM.TabIndex = 9;
+            // 
+            // txtKM
+            // 
+            this.txtKM.Location = new System.Drawing.Point(587, 180);
+            this.txtKM.Name = "txtKM";
+            this.txtKM.Size = new System.Drawing.Size(133, 25);
+            this.txtKM.TabIndex = 10;
+            // 
+            // txtIn
+            // 
+            this.txtIn.Location = new System.Drawing.Point(37, 304);
+            this.txtIn.Name = "txtIn";
+            this.txtIn.Size = new System.Drawing.Size(133, 25);
+            this.txtIn.TabIndex = 11;
+            // 
+            // txtFt
+            // 
+            this.txtFt.Location = new System.Drawing.Point(289, 304);
+            this.txtFt.Name = "txtFt";
+            this.txtFt.Size = new System.Drawing.Size(133, 25);
+            this.txtFt.TabIndex = 12;
+            // 
+            // txtYard
+            // 
+            this.txtYard.Location = new System.Drawing.Point(587, 304);
+            this.txtYard.Name = "txtYard";
+            this.txtYard.Size = new System.Drawing.Size(133, 25);
+            this.txtYard.TabIndex = 13;
+            // 
+            // BT1
+            // 
+            this.BT1.AutoSize = true;
+            this.BT1.Font = new System.Drawing.Font("新細明體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.BT1.Location = new System.Drawing.Point(32, 356);
+            this.BT1.Name = "BT1";
+            this.BT1.Size = new System.Drawing.Size(75, 30);
+            this.BT1.TabIndex = 14;
+            this.BT1.Text = "說明";
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Font = new System.Drawing.Font("MV Boli", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInfo.Location = new System.Drawing.Point(37, 393);
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.Size = new System.Drawing.Size(134, 45);
+            this.txtInfo.TabIndex = 15;
+            // 
+            // BT2
+            // 
+            this.BT2.BackColor = System.Drawing.SystemColors.Info;
+            this.BT2.Font = new System.Drawing.Font("新細明體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.BT2.Location = new System.Drawing.Point(421, 27);
+            this.BT2.Name = "BT2";
+            this.BT2.Size = new System.Drawing.Size(250, 64);
+            this.BT2.TabIndex = 16;
+            this.BT2.Text = "按下有驚喜";
+            this.BT2.UseVisualStyleBackColor = false;
+            this.BT2.Click += new System.EventHandler(this.BT2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(743, 450);
+            this.Controls.Add(this.BT2);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.BT1);
+            this.Controls.Add(this.txtYard);
+            this.Controls.Add(this.txtFt);
+            this.Controls.Add(this.txtIn);
+            this.Controls.Add(this.txtKM);
+            this.Controls.Add(this.txtM);
+            this.Controls.Add(this.txtCM);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAllClear);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAllClear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtCM;
+        private System.Windows.Forms.TextBox txtM;
+        private System.Windows.Forms.TextBox txtKM;
+        private System.Windows.Forms.TextBox txtIn;
+        private System.Windows.Forms.TextBox txtFt;
+        private System.Windows.Forms.TextBox txtYard;
+        private System.Windows.Forms.Label BT1;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.Button BT2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _004
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCM_KeyUp(object sender, KeyEventArgs e)
+        {
+            double douCM; //宣告一個double變數，變數名稱叫douCM
+
+            douCM = Convert.ToDouble(txtCM.Text); //從txtCM輸入文字框取得輸入的文字，並且轉換成double的資料型態
+
+            txtM.Text = string.Format("{0:0.##########}", douCM / 100);
+            txtKM.Text = string.Format("{0:0.##########}", douCM / 100000);
+            txtIn.Text = string.Format("{0:0.##########}", douCM / 2.54);
+            txtFt.Text = string.Format("{0:0.##########}", douCM / 30.48);
+            txtYard.Text = string.Format("{0:0.##########}", douCM / 91.44);
+
+        }
+
+        private void BT2_Click(object sender, EventArgs e)
+        {
+            txtInfo.Text = "WHAT?";
+        }
+
+        private void btnAllClear_Click(object sender, EventArgs e)
+        {
+            txtCM.Text = "";
+            txtM.Text = "";
+            txtKM.Text = "";
+            txtIn.Text = "";
+            txtFt.Text = "";
+            txtYard.Text = "";
+        }
+    }
+}
